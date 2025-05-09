50cf660fb6a1b0d2c3b807a7b9b0972f90dd6c9c
sivaprasad2848 <sivaprasad@cybersquare.org>
2025-05-09 12:17:09 +0530

    Git Ignore file added and tabulate package code inserted in display functionality

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 contacts=[('sam','sam@gmail.com','723767676'),('amal','amal@gmail.com','65236565')]
 def menu():
     print("Contact Manager")
@@ -15,8 +16,11 @@ def create_contact():
     contacts.append((name,email,mobile))
 def display_contact():
     #print(contacts)
+    table1=[]
     for item in contacts:
-        print(item[0]+" "+item[1]+" "+item[2])
+        #print(item[0]+" "+item[1]+" "+item[2])
+        table1.append([item[0],item[1],item[2]])
+    print(tabulate(table1, headers=['Name', 'Email','Mobile']))
 def edit_contact():
     display_contact()
     i=int(input("Enter the record index you want to Edit "))  
